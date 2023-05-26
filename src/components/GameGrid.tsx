ee1a52188458cf638b8cc126a18210be1e052b7a
Mahmudul Alam <mahmudula2000@gmail.com>
2023-05-26 13:25:08 +0600

    Refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,7 +2,6 @@ import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames.tsx";
 import GameCard from "./GameCard.tsx";
 import GameCardSkeleton from "./GameCardSkeleton.tsx";
-import {Fragment} from "react";
 import GameCardContainer from "./GameCardContainer.tsx";
 import {GameQuery} from "../App";
 
@@ -13,27 +12,26 @@ interface Props {
 const GameGrid = ({gameQuery}: Props) => {
     const {data: games, error, isLoading} = useGames(gameQuery);
     const skeletons = Array(20).fill(null);
+
+    if (error) return <Text>{error}</Text>;
     return (
-        <Fragment>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{
-                sm: 1,
-                md: 2,
-                lg: 3,
-                xl: 4
-            }} padding={"10px"} spacing={6}>
-                {isLoading && skeletons.map((_, index) => (
-                    <GameCardContainer key={index}>
-                        <GameCardSkeleton/>
-                    </GameCardContainer>
-                ))}
-                {!isLoading && games.map(game => (
-                    <GameCardContainer key={game.id}>
-                        <GameCard game={game}/>
-                    </GameCardContainer>
-                ))}
-            </SimpleGrid>
-        </Fragment>
+        <SimpleGrid columns={{
+            sm: 1,
+            md: 2,
+            lg: 3,
+            xl: 4
+        }} padding={"10px"} spacing={6}>
+            {isLoading && skeletons.map((_, index) => (
+                <GameCardContainer key={index}>
+                    <GameCardSkeleton/>
+                </GameCardContainer>
+            ))}
+            {!isLoading && games.map(game => (
+                <GameCardContainer key={game.id}>
+                    <GameCard game={game}/>
+                </GameCardContainer>
+            ))}
+        </SimpleGrid>
     );
 };
 
